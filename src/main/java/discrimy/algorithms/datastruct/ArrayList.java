63c066a1b33946cdf7cf910cf15d69f21d8b9323
discrimy <discrimy.off@gmail.com>
2019-08-13 19:42:41 +0300

    Add array based list implementation

---
+++ src/main/java/discrimy/algorithms/datastruct/ArrayList.java
@@ -0,0 +1,102 @@
+package discrimy.algorithms.datastruct;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class ArrayList<T> implements List<T> {
+    private int size = 0;
+    private T[] array;
+
+    private void requireIndexInBound(int index) {
+        if (index >= size) throw new IndexOutOfBoundsException("Size is " + size + " but index is " + index);
+    }
+
+    private void changeArraySize(int newSize) {
+        @SuppressWarnings("unchecked")
+        T[] newArray = (T[]) new Object[newSize];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
+
+    public ArrayList() {
+        //noinspection unchecked
+        array = (T[]) new Object[8];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(T data) {
+        if (size == array.length) {
+            changeArraySize(array.length + 8);
+        }
+
+        array[size] = data;
+        size++;
+    }
+
+    @Override
+    public T get(int index) {
+        requireIndexInBound(index);
+        return array[index];
+    }
+
+    @Override
+    public void remove(int index) {
+        requireIndexInBound(index);
+        if (size - index + 1 >= 0) System.arraycopy(array, index + 1, array, index, size - index + 1);
+        size--;
+
+        if (size == array.length - 8) {
+            changeArraySize(size - 8);
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(get(i), o)) {
+                remove(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(get(i), o)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Optional<Integer> indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(get(i), o)) {
+                return Optional.of(i);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (Objects.equals(get(i), o)) {
+                return Optional.of(i);
+            }
+        }
+
+        return Optional.empty();
+    }
+}
---
+++ src/test/java/discrimy/alrothims/ArrayListTest.java
@@ -0,0 +1,71 @@
+package discrimy.alrothims;
+
+import discrimy.algorithms.datastruct.ArrayList;
+import discrimy.algorithms.datastruct.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayListTest {
+    List<Integer> list;
+
+    @BeforeEach
+    public void prepare() {
+        list = new ArrayList<>();
+        list.add(1);
+        list.add(5);
+        list.add(3);
+    }
+
+    @Test
+    public void get() {
+        assertEquals(3, list.size());
+        assertEquals(Integer.valueOf(1), list.get(0));
+        assertEquals(Integer.valueOf(5), list.get(1));
+        assertEquals(Integer.valueOf(3), list.get(2));
+    }
+
+    @Test
+    public void removeIndex() {
+        list.remove(1);
+
+        assertEquals(2, list.size());
+        assertEquals(Integer.valueOf(1), list.get(0));
+        assertEquals(Integer.valueOf(3), list.get(1));
+    }
+
+    @Test
+    public void removeValue() {
+        boolean isRemoved = list.remove(Integer.valueOf(5));
+
+        assertTrue(isRemoved);
+        assertEquals(2, list.size());
+        assertEquals(Integer.valueOf(1), list.get(0));
+        assertEquals(Integer.valueOf(3), list.get(1));
+    }
+
+    @Test
+    public void contains() {
+        boolean containsTrue = list.contains(5);
+        boolean containsFalse = list.contains(-1);
+
+        assertTrue(containsTrue);
+        assertFalse(containsFalse);
+    }
+
+    @Test
+    public void indexOf() {
+        Optional<Integer> index = list.indexOf(5);
+        Optional<Integer> lastIndex = list.lastIndexOf(1);
+        Optional<Integer> noIndex = list.indexOf(-1);
+        Optional<Integer> noLastIndex = list.lastIndexOf(-1);
+
+        assertEquals(Optional.of(1), index);
+        assertEquals(Optional.of(0), lastIndex);
+        assertEquals(Optional.empty(), noIndex);
+        assertEquals(Optional.empty(), noLastIndex);
+    }
+}
